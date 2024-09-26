ced2068d5ed57ec7311f826c149b31945f357ce3
xiaokouxyz <91874335+xiaokouxyz@users.noreply.github.com>
2024-09-26 15:17:57 +0800

    feat: sdk init

---
+++ business-behavior-monitor-sdk/src/main/java/com/kou/monitor/sdk/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author KouJY
+ * Date: 2024/9/26 15:16
+ * Package: com.kou.monitor.sdk
+ */
+package com.kou.monitor.sdk;
---
+++ business-behavior-monitor-sdk/src/test/java/com/kou/monitor/sdk/test/ApiTest.java
@@ -0,0 +1,9 @@
+package com.kou.monitor.sdk.test;
+
+/**
+ * @author KouJY
+ * Date: 2024/9/26 15:17
+ * Package: com.kou.monitor.sdk.test
+ */
+public class ApiTest {
+}
